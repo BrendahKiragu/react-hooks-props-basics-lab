9f85f69607737e5bd27dbc6fdd3b86a686ae1062
Brendah Mwihaki Kiragu <brendakiragu02@gmail.com>
2024-07-31 17:50:47 +0300

    adds bio and links to about component

--- src/components/About.js
+++ src/components/About.js
@@ -1,14 +1,24 @@
 import React from "react";
+import Links from "./Links";
 
-function About() {
+function About(props) {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+
+      {/* conditional rendering */}
+      {/* to check whether bio exists */}
+      {/* we wrap in curly braces cause it a jsx operation */}
+      {/* as long as what is on left is true, what is on right will be displayed */}
+      {props.bio && <p>{props.bio}</p>}
+      
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+
+      <Links github={props.links.github} linkedin={props.links.linkedin} />
     </div>
   );
 }
 
 export default About;
+
+//About is parent to Links hence takes the actual value
